60e5af3734a603199a81823d7434b300b065e026
Artem Baguinski <abaguinski@depraktijkindex.nl>
2013-06-17 08:39:03 +0200

    generated a new rails app
    rails new demo-1 -d postgresql -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo1::Application.config.secret_token = '0a76f1bad1c4139668d601a07f17ab04dab0ebcee0e9cd0d2bad4018ae47e0fcadcde7501bb8ecc8f6f66ca63bb9dca468ca626d2bf9fb89c29defe1370fe785'
