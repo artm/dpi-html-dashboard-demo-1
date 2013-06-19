6d66363a65460471c21d85e1f9931e909f68f749
Artem Baguinski <abaguinski@depraktijkindex.nl>
2013-06-19 17:25:37 +0200

    associacies
    Normaal wordt model naam gebruikt voor associaties, maar in geval van
    'HospitalCondition' / 'stats' neig ik tot aparte naamgeving. Misschien
    ben ik gewoon in oncreatieve bui (om een beter alternatief te verzinnen
    die op allebei plekken past).
    
    Associacies helpen aan ruby kant duidelijk te maken hoe tabellen /
    modellen met elkaar in verband staan. Hoe ze dat doen wordt duidelijker
    in de volgende commit. Hopelijk.

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,3 +1,5 @@
 class Condition < ActiveRecord::Base
   attr_accessible :title
+
+  has_many :stats, class_name: 'HospitalCondition'
 end
--- app/models/hospital_condition.rb
+++ app/models/hospital_condition.rb
@@ -1,3 +1,6 @@
 class HospitalCondition < ActiveRecord::Base
   attr_accessible :condition_id, :eligible_discharges, :hospital_acquired_conditions, :hospital_id
+
+  belongs_to :hospital
+  belongs_to :condition
 end
