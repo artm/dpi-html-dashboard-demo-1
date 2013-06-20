9a371a5cca03e4d5542da6bc16ffdcce283f189c
Artem Baguinski <abaguinski@depraktijkindex.nl>
2013-06-20 11:58:05 +0200

    also skip timestamps in joined objects

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,6 @@
 class ApiController < ApplicationController
   def hospitals
-    list_model Hospital, include: :stats
+    list_model Hospital, include: { stats: { except: TimeStamps } }
   end
 
   def conditions
@@ -14,8 +14,10 @@ class ApiController < ApplicationController
   private
 
   def list_model model, options = {}
-    options = options.deep_merge(except: %w(created_at updated_at))
+    options = options.deep_merge(except: TimeStamps)
     data_set = model.limit(params[:limit]).offset(params[:offset])
     render json: data_set.to_json( options )
   end
+
+  TimeStamps = %w(created_at updated_at)
 end
