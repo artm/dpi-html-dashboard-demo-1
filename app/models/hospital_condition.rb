d766244b34724358ee9460d841b501ba80b4a687
Artem Baguinski <abaguinski@depraktijkindex.nl>
2013-06-20 11:15:13 +0200

    een voorbeeld van afgeleide kolom
    rate_per_1000 is een kolom die in ruby wordt uitgerekend en in admin
    getoond, maar niet in de database opgeslagen.

--- app/models/hospital_condition.rb
+++ app/models/hospital_condition.rb
@@ -4,4 +4,17 @@ class HospitalCondition < ActiveRecord::Base
 
   belongs_to :hospital
   belongs_to :condition
+
+  def rate_per_1000
+    if eligible_discharges == 0
+      0
+    else
+      1000.0 * hospital_acquired_conditions / eligible_discharges rescue nil
+    end
+  end
+
+  rails_admin do
+    configure :rate_per_1000, :float
+    exclude_fields :created_at, :updated_at
+  end
 end
